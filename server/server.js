cb6c5c5b168b4745973dc5f06503eab8b7e9a728
horis233 <horis233@gmail.com>
2018-09-10 13:05:13 -0400

    add mongodb uri

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://horis:hjm123@ds161059.mlab.com:61059/lyric-maker-db';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
