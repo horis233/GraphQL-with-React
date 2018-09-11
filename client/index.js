f80fec0e5cc29e1ca6edcef8b60ea54692e9c0b2
horis233 <horis233@gmail.com>
2018-09-11 00:28:59 -0400

    add Apollo into Project

--- client/index.js
+++ client/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
-import { ApolloProvider } from 'react-apollo';
+import {ApolloProvider} from 'react-apollo';
 
 import SongList from './components/SongList';
 
 const client = new ApolloClient({});
 
 const Root = () => {
-  return
-  <ApolloProvider client = {client}>
+  return <ApolloProvider client={client}>
     <SongList/>
   </ApolloProvider>
 };
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Root/>, document.querySelector('#root'));
