a473dd3754f9a2f21fb8e57c22589bf2ef3ffc72
horis233 <horis233@gmail.com>
2018-09-14 17:19:40 -0400

    add css file

--- client/index.js
+++ client/index.js
@@ -7,6 +7,8 @@ import {ApolloProvider} from 'react-apollo';
 import App from './components/App';
 import SongList from './components/SongList';
 import SongCreate from './components/SongCreate';
+import './style/style.css';
+
 const client = new ApolloClient({
   dataIdFromObject: o => o.id
 });
