25dabc9b3618222bbaade8ca99857cd5f1eedd0a
horis233 <horis233@gmail.com>
2018-09-15 17:03:56 -0400

    created fetch song query

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import fetchSong from '../queries/fetchSong';
 
 class SongDetail extends Component{
   render(){
     return (
       <div>
-        <h3>
-          Song Detail
-        </h3>
+        <Link to="/">Back</Link>
+        <h3>{song.title}</h3>
       </div>
     );
   }
 }
 
-export default SongDetail
+export default graphql(fetchSong, {
+  options: (props) => { return { variables: { id: props.params.id } } }
+})(SongDetail);
