9bc44b54db815cb6cc2da735684f871c7ac2de05
horis233 <horis233@gmail.com>
2018-09-15 18:14:20 -0400

    added Lyric list on song detail

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -3,16 +3,25 @@ import React, { Component } from 'react';
 
 class LyricList extends Component {
 
+  renderLyrics() {
+    return this.props.lyrics.map(({ id, content}) => {
+      return (
+        <li key={id} className="collection-item">
+          {content}
+        </li>
+      );
+    });
+  }
 
 
   render() {
     return (
-      <ul >
-        LyricList
+      <ul className="collection">
+        {this.renderLyrics()}
       </ul>
     );
   }
 }
 
 
-export default graphql(mutation)(LyricList);
+export default LyricList;
