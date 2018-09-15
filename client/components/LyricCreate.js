9e713b163911f75aa5d087b76a5dffbbfa9d71b0
horis233 <horis233@gmail.com>
2018-09-15 17:47:53 -0400

    created Lyric create

---
+++ client/components/LyricCreate.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+class LyricCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { content: '' };
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit.bind(this)}>
+        <label>Add a Lyric</label>
+        <input
+          value={this.state.content}
+          onChange={event => this.setState({ content: event.target.value })}
+        />
+      </form>
+    );
+  }
+}
+
+
+
+export LyricCreate;
